5690e255ebd0e64f64091134c70671c61aceb8a5
EHS2007 <milaroco3004@gmail.com>
2024-03-15 20:00:19 -0500

    first commit

---
+++ A Tutorial Introduction/Getting Started/helloWorld.c
@@ -0,0 +1,9 @@
+// To compile this file you should write "gcc -o helloWorld helloWorld.c"
+// To run the file you should use "./helloWorld"
+/// A function includes statements
+/// A variables includes values used during computation
+#include <stdio.h> // Include the information from the standard input and output library
+
+int main() {  // --------> define a function main with no arguments enclosed in braces
+  printf("Hello World!\n"); // a printf function to print a sequnce of characters, \n represents a newline character
+}
